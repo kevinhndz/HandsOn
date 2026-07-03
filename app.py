d377f69c5a0d65c0b443d8e3e948662417d71054
Kevin <kh925063@gmail.com>
2026-07-02 22:25:11 -0600

    Protocolo HTTPS = GET

--- app.py
+++ app.py
@@ -1,16 +1,46 @@
 from flask import Flask, render_template
 
+
 app = Flask(__name__)
 
 
 @app.route('/')
 def home():
     
-    user = "Kevin Hernandez"
-    return render_template('index.html', user=user)
+    
+    hay = True
+    contenido = {}
+    
+    if hay == True:
+        contenido = {
+        "Name": "Kevin",
+        "City": "Comayagua",
+        "Age": 26,
+        "Message":"Bienvenido"
+        
+    }
+    else:
+        contenido = {
+            "Message": "No hay Contenido en la base de datos"
+        }
+        hay = False
+        
+        
+    
+    
+    
+    return render_template('index.html',contenido= contenido, hay = hay)
+
+
+@app.route('/proyects')
+def proyectos():
+    projectos = ["Git", "Docker"]
+    
+    return render_template('proyects.html',projectos = projectos)
 
 
 
 
-if __name__ == '__main__':
+
+if __name__ == "__main__":
     app.run(debug=True)
