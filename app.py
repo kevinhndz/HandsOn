5f3f227109a8f240d1bdbcfa0f37366acff9362f
Kevin <kh925063@gmail.com>
2026-07-03 15:33:27 -0600

    Otro metodo HTTP

--- app.py
+++ app.py
@@ -1,34 +1,36 @@
-from flask import Flask, render_template, request
+from flask import Flask, render_template,request
 
 app = Flask(__name__)
 
-@app.route("/")
-def home():
-    return render_template("index.html")
+@app.route ('/')
+def main():
+    return render_template('index.html')
 
-@app.route("/atributos", methods=["POST"])
+
+@app.route('/cualidades', methods = ["POST"])
 def atributos():
-    kevin = {"Edad": 26, "Equipo": "Portugal", "Sexo": "M"}
-    maria = {"Edad": 19, "Equipo": "Francia", "Sexo": "F"}
-    oscar = {"Edad": 22, "Equipo": "Inglaterra", "Sexo": "M"}
-
-    campo = request.form.get("campo")
-
-    match campo:
-        case "kevin":
-            datos = kevin
-        case "maria":
-            datos = maria
-        case "oscar":
-            datos = oscar
-        case _:
-            datos = {"Error": "No encontrado"}
-
-    return render_template(
-        "atributos.html",
-        nombre=campo,
-        datos=datos
-    )
-
+    
+    name = request.form.get("nombre")
+    posicion_jugador = request.form.get("campo")
+    es_capitan = request.form.get("capitan")
+    
+    if es_capitan == None:
+        es_capitan = "Jugador de Campo"
+    else:
+        es_capitan = "Es Capitan"
+    
+    diccionario = {
+        
+        "Nombre": name,
+        "Posicion": posicion_jugador,
+        "Capitan": es_capitan
+    }
+    
+    
+    nombre= diccionario["Nombre"]
+    return render_template('atributos.html',diccionario = diccionario, nombre = nombre)
+    
+    
+    
 if __name__ == "__main__":
     app.run(debug=True)
