8da44a54570765a00f4b8310565df754b9d2818e
Kevin <kh925063@gmail.com>
2026-07-03 01:12:59 -0600

    https: POST

--- app.py
+++ app.py
@@ -1,16 +1,34 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 
 app = Flask(__name__)
 
-
-@app.route('/')
+@app.route("/")
 def home():
-    
-    user = "Kevin Hernandez"
-    return render_template('index.html', user=user)
+    return render_template("index.html")
 
+@app.route("/atributos", methods=["POST"])
+def atributos():
+    kevin = {"Edad": 26, "Equipo": "Portugal", "Sexo": "M"}
+    maria = {"Edad": 19, "Equipo": "Francia", "Sexo": "F"}
+    oscar = {"Edad": 22, "Equipo": "Inglaterra", "Sexo": "M"}
 
+    campo = request.form.get("campo")
 
+    match campo:
+        case "kevin":
+            datos = kevin
+        case "maria":
+            datos = maria
+        case "oscar":
+            datos = oscar
+        case _:
+            datos = {"Error": "No encontrado"}
 
-if __name__ == '__main__':
+    return render_template(
+        "atributos.html",
+        nombre=campo,
+        datos=datos
+    )
+
+if __name__ == "__main__":
     app.run(debug=True)
